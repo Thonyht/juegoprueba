44e5e382c116419233c7388ba4cd99cbe6c4e38e
Thony Herrera <thonyherrera92@gmail.com>
2022-06-02 13:42:41 -0400

    prueba de otro ambiente

--- javadoc/src/javadoc/Main.java
+++ javadoc/src/javadoc/Main.java
@@ -1,10 +1,13 @@
 package javadoc;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner leer = new Scanner(System.in);
+		System.out.println("dato nuevo");
 
 	}
 
